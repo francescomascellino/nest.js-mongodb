269c3d51b7e6bfbc0b38934ad6a7a37813acfc0a
francesco.mascellino <foggolo@hotmail.com>
2024-06-19 09:09:25 +0200

    Install CORS dependencies

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configura CORS per accettare solo le richieste dall'URL specificato
+  app.enableCors({
+    origin: 'http://localhost:5173', // Imposta l'URL del tuo frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Metodi HTTP consentiti
+    allowedHeaders: ['Content-Type', 'Authorization'], // Intestazioni consentite
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true, // Accetta i cookie e l'header Authorization
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       // I payload sono oggetti JS. Abilitiamo la trasformazione automatica globale per tipicizzare questi oggetti in base alla loro classe DTO
